0810cdc8d27abb8168bcdecd88348a0bdbd43165
Rafael Waldo Delgado Doblas <cuentas.rwd@rawapro.com>
2022-05-29 14:23:20 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 rm -r "$HOME/bin/AndroidStudioOnDocker.sh"
 rm -r "$HOME/.local/share/icons/hicolor/scalable/apps/studio-docker.svg"
 rm -r "$HOME/.local/share/applications/android-studio-on-docker.desktop"
-rm -r "$HOME/.AndroidStudioOnDocker"
+rm -r "$HOME/.onDocker/AndroidStudio"
 
 docker image rm lordrafa/android-studio
 
